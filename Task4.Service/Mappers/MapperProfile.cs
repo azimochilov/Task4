d56c43d10c6b025eb40516af27b72a76f08153f2
azimochilov <azimochilov226@gmail.com>
2023-06-06 16:53:29 +0300

    added mapperprofile

---
+++ Task4.Service/Mappers/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Task4.Domain.Entities;
+using Task4.Service.DTOs;
+
+namespace Task4.Service.Mappers;
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<User, UserForCreationDto>().ReverseMap();
+        CreateMap<User, UserForResultDto>().ReverseMap();
+        CreateMap<User, UserForUpdateDto>().ReverseMap();
+
+    }
+}
