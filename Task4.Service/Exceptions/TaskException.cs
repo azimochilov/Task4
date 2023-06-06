ac1b87338da08c4de8687ec629e639f5c84bdf15
azimochilov <azimochilov226@gmail.com>
2023-06-06 15:10:30 +0300

    exceptions is implemented

---
+++ Task4.Service/Exceptions/TaskException.cs
@@ -0,0 +1,9 @@
+﻿namespace Task4.Service.Exceptions;
+public class TaskException : Exception
+{
+    public int Code { get; set; }
+    public TaskException(int code = 500, string message = "Something went wrong") : base(message)
+    {
+        this.Code = code;
+    }
+}
