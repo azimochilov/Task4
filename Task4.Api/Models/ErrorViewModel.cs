6cb29d09a9afae9324a9cf7c1df3ec01747a3418
azimochilov <azimochilov226@gmail.com>
2023-06-06 12:28:28 +0300

    Api layer added

---
+++ Task4.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task4.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
