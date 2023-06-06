ed122615480d5826a48a66fa74093c5d76429d25
azimochilov <azimochilov226@gmail.com>
2023-06-06 16:42:19 +0300

    update dto is done for user

---
+++ Task4.Service/DTOs/UserForUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task4.Service.DTOs;
+public class UserForUpdateDto
+{
+
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Please enter valid email")]
+    public string Email { get; set; }
+}
