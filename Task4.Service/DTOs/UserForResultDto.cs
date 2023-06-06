7e5c85b23f9e332e39d3505c95bc4812b7c1c054
azimochilov <azimochilov226@gmail.com>
2023-06-06 16:34:17 +0300

    added userresult dto

---
+++ Task4.Service/DTOs/UserForResultDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Task4.Service.DTOs;
+public class UserForResultDto
+{
+    [DisplayName("First Name")]
+    public string Name { get; set; }
+    
+    public string Email { get; set; }
+}
