1b20f2b3491af4d968abb760e2d4096e6033ef66
azimochilov <azimochilov226@gmail.com>
2023-06-06 11:24:34 +0300

    auditable is implemented in commons file

---
+++ Task4.Domain/Commons/Auditable.cs
@@ -0,0 +1,11 @@
+﻿namespace Task4.Domain.Commons;
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public long? UpdatedBy { get; set; }
+    public long? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+
+}
