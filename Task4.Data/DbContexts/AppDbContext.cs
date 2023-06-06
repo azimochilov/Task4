dbd1e7406fb23c324075376302eedb7e65a91109
azimochilov <azimochilov226@gmail.com>
2023-06-06 12:10:22 +0300

    IRepository is used for implementing Repository

---
+++ Task4.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task4.Domain.Entities;
+
+namespace Task4.Data.DbContexts;
+public class AppDbContext : DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+}
---
+++ Task4.Data/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Task4.Data.IRepositories;
+using Task4.Domain.Commons;
+
+namespace Task4.Data.Repositories;
+public class Repository<T> : IRepository<T> where T : Auditable
+{
+    public ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<T> InsertAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
