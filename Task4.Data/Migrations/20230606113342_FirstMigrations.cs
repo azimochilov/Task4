5f3331bed0bb8a222c600835f885dbf3f35402fe
azimochilov <azimochilov226@gmail.com>
2023-06-06 14:44:35 +0300

    fixed bugs and updated databae

---
+++ Task4.Data/Migrations/20230606113342_FirstMigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task4.Data.Migrations
+{
+    public partial class FirstMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
