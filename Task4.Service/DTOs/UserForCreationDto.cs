466eed9b688e7009deae95c2cd48222d668a24db
azimochilov <azimochilov226@gmail.com>
2023-06-06 16:28:46 +0300

    added Creation user dto

---
+++ Task4.Service/DTOs/UserForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task4.Service.DTOs;
+public class UserForCreationDto
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Please enter valid email")]
+    public string Email { get; set; }
+}
