e1a1467bf8d245df6740d0a5941888c4f889e68f
azimochilov <azimochilov226@gmail.com>
2023-06-10 13:44:32 +0300

    implemented methods for user interface

--- Task4.Service/Interfaces/IUserService.cs
+++ Task4.Service/Interfaces/IUserService.cs
@@ -1,5 +1,10 @@
-﻿namespace Task4.Service.Interfaces;
+﻿using Task4.Service.DTOs;
+
+namespace Task4.Service.Interfaces;
 public interface IUserService
 {
-
+    Task<UserForResultDto> AddAsync(UserForCreationDto userForCreationDto);
+    Task<bool> DeleteAsync(long id);
+    Task<UserForResultDto> BlockAsync(long id);
+    Task<UserForResultDto> UnBlockAsync(long id);
 }
