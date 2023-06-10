d6cadd6d262097e62842b5fb5ef42c002f9796aa
azimochilov <azimochilov226@gmail.com>
2023-06-10 17:44:37 +0300

    some changes on appsettings.json

---
+++ Task4.Api/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task4.Service.DTOs;
+using Task4.Service.Interfaces;
+
+namespace Task4.Api.Controllers;
+
+public class AccountController : Controller
+{
+    private IUserService userService;
+    private IConfiguration configuration;
+    private IAuthenticationService authenticationService;
+
+    public AccountController(IAuthenticationService authenticationService, IUserService userService, IConfiguration configuration)
+    {
+        this.userService = userService;
+        this.configuration = configuration;
+        this.authenticationService = authenticationService;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    // POST: Register
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Register(UserForCreationDto creationDto)
+    {
+        if (ModelState.IsValid)
+        {
+            var createdUser = await this.userService.AddAsync(creationDto);
+
+            if (createdUser is null)
+            {
+                ModelState.AddModelError("", "This email is already used.");
+                return View(creationDto);
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        return View(creationDto);
+    }
+}
