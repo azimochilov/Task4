d720f98777949fc0f2a780216f07abfe26ac09c7
azimochilov <azimochilov226@gmail.com>
2023-06-10 16:25:22 +0300

    added some methods

--- Task4.Api/Extensions/ServiceExtensions.cs
+++ Task4.Api/Extensions/ServiceExtensions.cs
@@ -1,5 +1,20 @@
-﻿namespace Task4.Api.Extensions;
-public class ServiceExtensions
+﻿using Task4.Data.IRepositories;
+using Task4.Data.Repositories;
+using Task4.Domain.Entities;
+using Task4.Service.Interfaces;
+using Task4.Service.Services;
+
+namespace Task4.Api.Extensions;
+public static class ServiceExtensions
 {
+    public static void AddCustomServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        services.AddScoped<IUserService, UserService>();        
+        services.AddScoped<IAuthorizationService, AuthorizationService>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+    }
+
 
 }
