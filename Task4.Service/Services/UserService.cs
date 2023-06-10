12665ea3a7770aeb31a1d852f80a3ed72e3d4747
azimochilov <azimochilov226@gmail.com>
2023-06-10 14:07:31 +0300

    added adding method for user service

--- Task4.Service/Services/UserService.cs
+++ Task4.Service/Services/UserService.cs
@@ -1,5 +1,48 @@
-﻿namespace Task4.Service.Services;
-public class UserService
-{
+﻿using AutoMapper;
+using Task4.Data.IRepositories;
+using Task4.Domain.Entities;
+using Task4.Service.DTOs;
+using Task4.Service.Exceptions;
+using Task4.Service.Interfaces;
 
+namespace Task4.Service.Services;
+public class UserService : IUserService
+{
+    private readonly IMapper mapper;
+    private readonly IRepository<User> repository;
+
+    public UserService(IMapper mapper, IRepository<User> repository)
+    {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+    public async Task<UserForResultDto> AddAsync(UserForCreationDto userForCreationDto)
+    {
+        var exsistEntity = await repository.SelectAsync(x => x.Email == userForCreationDto.Email && !x.IsDeleted);
+        if(exsistEntity is null)
+        {
+            throw new TaskException(409, "User already exsist!");
+        }
+
+        var mapped = mapper.Map<User>(userForCreationDto);
+        var addedModel = await repository.InsertAsync(mapped);
+        await repository.SaveAsync();
+        
+        return mapper.Map<UserForResultDto>(addedModel);
+    }
+
+    public Task<UserForResultDto> BlockAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserForResultDto> UnBlockAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
 }
