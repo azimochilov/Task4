5c2f9fa49a4360ea7e98bb2b11924b4b050d7547
azimochilov <azimochilov226@gmail.com>
2023-06-10 14:49:38 +0300

    change small

--- Task4.Service/Interfaces/IAuthenticationService.cs
+++ Task4.Service/Interfaces/IAuthenticationService.cs
@@ -1,5 +1,7 @@
-﻿namespace Task4.Service.Interfaces;
+﻿using Task4.Service.DTOs;
+
+namespace Task4.Service.Interfaces;
 public interface IAuthenticationService
 {
-
+    Task<string> AuthenticateAsync(UserForResultDto user);
 }
---
+++ Task4.Service/Services/AuthenticationService.cs
@@ -0,0 +1,7 @@
+﻿using Task4.Service.Interfaces;
+
+namespace Task4.Service.Services;
+public class AuthenticationService : IAuthenticationService
+{
+
+}
