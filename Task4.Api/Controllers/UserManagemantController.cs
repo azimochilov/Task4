7dad586214406924e5663007e7004b7ff04d39bb
azimochilov <azimochilov226@gmail.com>
2023-06-11 22:05:50 +0300

    some changes

---
+++ Task4.Api/Controllers/UserManagemantController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task4.Service.Interfaces;
+
+namespace Task4.Api.Controllers;
+public class UserManagemantController : Controller
+{
+    private IUserService userService;
+    private IAuthorizationService authorization;
+
+    public UserManagemantController(IUserService userService, IAuthorizationService authorization)
+    {
+        this.userService = userService;
+        this.authorization = authorization;
+    }
+
+    // GET: UserManagement
+    public async Task<ActionResult> Index()
+    {
+        var authResult = await authorization.AuthorizeAsync();
+
+        if (!authResult)
+            return RedirectToAction("Login", "Account");
+
+        var users = await this.userService.GetAllAsync();
+
+        return View(users);
+    }
+
+    // POST: BlockUsers
+    [HttpPost]
+    public async Task<ActionResult> BlockUsers(int[] userIds)
+    {
+        var authResult = await authorization.AuthorizeAsync();
+
+        if (!authResult)
+            return RedirectToAction("Login", "Account");
+
+        var token = Request.Cookies["AuthToken"];
+        foreach (var userId in userIds)
+            await this.userService.BlockAsync(userId);
+
+        return RedirectToAction("Index", "UserManagement");
+    }
+
+    // POST: UnblockUsers
+    [HttpPost]
+    public async Task<ActionResult> UnblockUsers(int[] userIds)
+    {
+        var authResult = await authorization.AuthorizeAsync();
+
+        if (!authResult)
+            return RedirectToAction("Login", "Account");
+
+        foreach (var userId in userIds)
+            await this.userService.UnBlockAsync(userId);
+
+        return RedirectToAction("Index", "UserManagement");
+    }
+
+    // POST: DeleteUsers
+    [HttpPost]
+    public async Task<ActionResult> DeleteUsers(int[] userIds)
+    {
+        var authResult = await authorization.AuthorizeAsync();
+
+        if (!authResult)
+            return RedirectToAction("Login", "Account");
+
+        foreach (var userId in userIds)
+            await this.userService.DeleteAsync(userId);
+
+        return RedirectToAction("Index", "UserManagement");
+    }
+}
--- Task4.Api/Program.cs
+++ Task4.Api/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddAutoMapper(typeof(MapperProfile));
 builder.Services.AddControllersWithViews();
 builder.Services.AddJwtService(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 var app = builder.Build();
