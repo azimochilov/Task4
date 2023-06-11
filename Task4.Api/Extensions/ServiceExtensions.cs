bc1e4c04428a75b3f7dd2b0000c29e60625c044a
azimochilov <azimochilov226@gmail.com>
2023-06-11 22:09:46 +0300

    some changes

--- Task4.Api/Extensions/ServiceExtensions.cs
+++ Task4.Api/Extensions/ServiceExtensions.cs
@@ -12,9 +12,10 @@ public static class ServiceExtensions
 {
     public static void AddCustomServices(this IServiceCollection services)
     {
-        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));     
 
-        services.AddScoped<IUserService, UserService>();        
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IUserService, UserService>();
         services.AddScoped<IAuthorizationService, AuthorizationService>();
         services.AddScoped<IAuthenticationService, AuthenticationService>();
     }
